d8aad4f1716ca6515bf9d986ca880b0f1fa30415
hippaforalkus <admin@psio.cba.pl>
2015-05-08 09:56:31 +0200

    kolko c++ poprawa tablic z 1-3 na 0-2

--- kolko/1.cpp
+++ kolko/1.cpp
@@ -5,11 +5,13 @@ int wczytaj(int A[3][3])
 int w=1;
 int x,y;
 while (w==1)
-    cout<<"podaj x "<<endl;
-    cin>>y;
+	cout<<"podaj x "<<endl;
+	cin>>y;
 	cout<<"podaj y"<<endl;
 	cin>>x;
-    if ((x<4)&(x>0)&(y<4)&(y>0))
+	x=x-1;
+	y=y-1;
+    if ((x<3)&(x>-1)&(y<3)&(y>-1))
     {
         if (A[x][y]==0)
         {
@@ -31,19 +33,19 @@ int spr(int A[3][3])
 int wynik=0;
 for (int i=1;i<=3;i++)
 {
-    if ((A[i][1]==A[i][2])&(A[i][1]==A[i][3])&(A[i][1]!=0))
+    if ((A[i][0]==A[i][1])&(A[i][0]==A[i][2])&(A[i][0]!=0))
         wynik=1;
-    if ((A[1][i]==A[2][i])&(A[1][i]==A[3][i])&(A[1][i]!=0))
+    if ((A[0][i]==A[1][i])&(A[0][i]==A[2][i])&(A[0][i]!=0))
         wynik=1;
 }
-if ((A[1][1]==A[2][2])&(A[1][1]==A[3][3])&(A[1][1]!=0))
+if ((A[0][0]==A[1][1])&(A[0][0]==A[2][2])&(A[0][0]!=0))
     wynik=1;
 
-if ((A[1][3]==A[2][2])&(A[1][3]==A[3][1])&(A[1][3]!=0))
+if ((A[0][2]==A[1][1])&(A[0][2]==A[2][0])&(A[0][2]!=0))
     wynik=1;
 
 }
-int wys(int A[8][8])
+int wys(int A[3][3])
 {
 }
 
